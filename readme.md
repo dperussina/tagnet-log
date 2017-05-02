0b3c5c36a79cc03cc898bf48c5d139640f882cc8
Daniel Perussina <dperussina@agentgrid.net>
2017-05-02 12:56:16 -0700

    Added .type .typeLog .logLn .errorLn

--- readme.md
+++ readme.md
@@ -9,9 +9,14 @@ npm install tagnet-log --save
 
 Usage:
 ```javascript
+/* Node.JS or in browser using Browserify */
 var u = require('tagnet-log');
+```
+General logging
+```javascript
 u.log('This is a test log message ', 'test', 3, [1, 2, 3]);
-u.error('This is a test error message ', 'test', 3, [1, 2, 3]);
+u.logLn('This is an inline test log message ', 'test', 3, [1, 2, 3]);
+
 ```
 Output:
 ```
@@ -29,6 +34,16 @@ test
 ]
 ###(u.log)###
 
+5/2/2017 12:44:22 PM This is an inline test log message  [string] test [number] 3 [array] [1,2,3] ###(u.logLn)###
+
+```
+Error logging
+```javascript
+u.error('This is a test error message ', 'test', 3, [1, 2, 3]);
+u.errorLn('This is an inline test error message ', 'test', 3, [1, 2, 3]);
+```
+Output:
+```
 3/30/2017 12:05:18 PM *Exception* 
 This is a test error message 
 [string] 
@@ -42,4 +57,31 @@ test
     3
 ]
 ###(u.error)###
+
+5/2/2017 12:45:33 PM *Exception*  This is an inline test error message  [string]  test [number]  3 [array]  [1,2,3] ###(u.errorLn)###
+
+```
+
+Types
+```javascript
+var myVal = ['I would like to know what Type this is'];
+    
+    
+    u.typeLog(myVal);
+    
+     
+    u.typeLog(myVal, 'myVal');
+
+    
+var thisType = u.type(myVal);    
+    console.log('thisType', thisType)
+```
+Output
+```
+5/2/2017 12:45:33 PM type [array] ###(u.typeLog)###
+
+5/2/2017 12:45:33 PM myVal | type [array] ###(u.typeLog)###
+
+thisType array
+
 ```
--- u.js
+++ u.js
@@ -1,94 +1,205 @@
-//var util = require('util');
-module.exports = {
-
-    log: function () {
-        var date = new Date();
-        var now =date.toLocaleDateString()+" "+ date.toLocaleTimeString();
-        var args = [];
-        for (var i = 0; i < arguments.length; i++) {
-            args[i] = arguments[i];
-        }
-        var cache = [];
-
-        var str = now + "";
-        args.forEach(function (e, i) {
-
-            if (typeof e != 'string') {
-
-                str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"]";
-                try{
-                    e = JSON.stringify(e, null, 4);
-                }catch(err){
-
-                }
-
-            }else{
-                if(i > 0){
-                    if(e.charAt(0) == "{"){
-                        try{
-
-                            e = JSON.stringify(JSON.parse(e), null, 4);
-                            str += "[json] ";
-
-                        }catch(err){
-                            str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"]";
-                        }
-                    }else{
-                        str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"]";
-                    }
 
 
-                }
+module.exports = (function () {
+    var u = {
+        log: function () {
+            var date = new Date();
+            var now = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            var args = [];
+            for (var i = 0; i < arguments.length; i++) {
+                args[i] = arguments[i];
             }
 
-            str+= "\r\n" + e + "\r\n";
-        });
-        console.log(str+'###(u.log)###');
 
-    },
-    error: function () {
-        var date = new Date();
-        var now =date.toLocaleDateString()+" "+ date.toLocaleTimeString();
-        var args = [];
-        for (var i = 0; i < arguments.length; i++) {
-            args[i] = arguments[i];
-        }
-        var cache = [];
+            var str = now + "";
+            args.forEach(function (e, i) {
 
-        var str = now+" *Exception* ";
-        args.forEach(function (e, i) {
+                if (typeof e != 'string') {
 
-            if (typeof e != 'string') {
+                    str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                    try {
+                        e = JSON.stringify(e, null, 4);
+                    } catch (err) {
 
-                str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"] ";
-                try{
-                    e = JSON.stringify(e, null, 4);
-                }catch(err){
-
-                }
-
-            }else{
-                if(i > 0){
-                    if(e.charAt(0) == "{"){
-                        try{
-
-                            e = JSON.stringify(JSON.parse(e), null, 4);
-                            str += "[json] ";
-
-                        }catch(err){
-                            str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"] ";
-                        }
-                    }else{
-                        str +="[" +({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() +"] ";
                     }
 
+                } else {
+                    if (i > 0) {
+                        if (e.charAt(0) == "{") {
+                            try {
 
+                                e = JSON.stringify(JSON.parse(e), null, 4);
+                                str += "[json] ";
+
+                            } catch (err) {
+                                str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                            }
+                        } else {
+                            str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                        }
+
+
+                    }
                 }
+
+                str += "\r\n" + e + "\r\n";
+            });
+            console.log(str + '###(u.log)###');
+            return null;
+        },
+        logLn: function () {
+            var date = new Date();
+            var now = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            var args = [];
+            for (var i = 0; i < arguments.length; i++) {
+                args[i] = arguments[i];
             }
 
-            str+= "\r\n" + e + "\r\n";
-        });
-        console.log(str+'###(u.error)###');
 
-    }
-};
+            var str = now + "";
+            args.forEach(function (e, i) {
+
+                if (typeof e != 'string') {
+
+                    str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                    try {
+                        e = JSON.stringify(e);
+                    } catch (err) {
+
+                    }
+
+                } else {
+                    if (i > 0) {
+                        if (e.charAt(0) == "{") {
+                            try {
+
+                                e = JSON.stringify(JSON.parse(e));
+                                str += "[json] ";
+
+                            } catch (err) {
+                                str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                            }
+                        } else {
+                            str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "]";
+                        }
+
+
+                    }
+                }
+
+                str += " " + e + " ";
+            });
+            console.log(str + '###(u.logLn)###');
+            return null;
+        },
+        error: function () {
+            var date = new Date();
+            var now = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            var args = [];
+            for (var i = 0; i < arguments.length; i++) {
+                args[i] = arguments[i];
+            }
+            var cache = [];
+
+            var str = now + " *Exception* ";
+            args.forEach(function (e, i) {
+
+                if (typeof e != 'string') {
+
+                    str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                    try {
+                        e = JSON.stringify(e, null, 4);
+                    } catch (err) {
+
+                    }
+
+                } else {
+                    if (i > 0) {
+                        if (e.charAt(0) == "{") {
+                            try {
+
+                                e = JSON.stringify(JSON.parse(e), null, 4);
+                                str += "[json] ";
+
+                            } catch (err) {
+                                str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                            }
+                        } else {
+                            str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                        }
+
+
+                    }
+                }
+
+                str += "\r\n" + e + "\r\n";
+            });
+            console.log(str + '###(u.error)###');
+            return null;
+        },
+        errorLn: function () {
+            var date = new Date();
+            var now = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            var args = [];
+            for (var i = 0; i < arguments.length; i++) {
+                args[i] = arguments[i];
+            }
+            var cache = [];
+
+            var str = now + " *Exception* ";
+            args.forEach(function (e, i) {
+
+                if (typeof e != 'string') {
+
+                    str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                    try {
+                        e = JSON.stringify(e);
+                    } catch (err) {
+
+                    }
+
+                } else {
+                    if (i > 0) {
+                        if (e.charAt(0) == "{") {
+                            try {
+
+                                e = JSON.stringify(JSON.parse(e));
+                                str += "[json] ";
+
+                            } catch (err) {
+                                str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                            }
+                        } else {
+                            str += "[" + ({}).toString.call(e).match(/\s([a-zA-Z]+)/)[1].toLowerCase() + "] ";
+                        }
+
+
+                    }
+                }
+
+                str += " " + e + " ";
+            });
+            console.log(str + '###(u.errorLn)###');
+            return null;
+        },
+        typeLog: function (arg, name) {
+            var date = new Date();
+            var now = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            var type = ({}).toString.call(arg).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+            var str = now + " ";
+            if(name){
+                str+=name+" | type ["+ type +"] ###(u.typeLog)###"
+            }else{
+                str+="type ["+ type +"] ###(u.typeLog)###"
+            }
+            console.log(str);
+            return type;
+        },
+        type: function (arg, name) {
+            var type = ({}).toString.call(arg).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+            return type;
+        }
+    };
+    return u;
+})();
+
